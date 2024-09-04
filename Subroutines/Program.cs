508c72f68e57ceb34f4b49188f6ad327a6d722ee
Al Stone <astone@heckgrammar.co.uk>
2024-09-04 14:09:49 +0100

    added a function

--- Subroutines/Program.cs
+++ Subroutines/Program.cs
@@ -12,16 +12,24 @@ namespace Subroutines
         {
             Console.WriteLine("Hello World"); // This is a procedure
             Console.WriteLine( Math.Pow(7,2)); // this is a function as it returns a value
-            sayHello(); // this is a subroutine
-            sayHello();
+            sayHello(); // this is a subroutine call
+            sayHello(); // this is another subroutine call
+            int b = giveMeANumber(); // this is a function call
+            Console.WriteLine(b);
         }
-        static void sayHello()
+        static void sayHello() // procedure (Returns nothing - void)
         {
             Console.WriteLine("Hello");
             Console.WriteLine("Look at me!");
             Console.WriteLine("I am a subroutine");
 
         }
-       
+
+        static int giveMeANumber() // function (Returns a value - int)
+        {
+            return 42;
+
+        }
+
     }
 }
