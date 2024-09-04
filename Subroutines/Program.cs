1881b9db4366a33bd923002807a555c4cfdd3944
astone <astone@B103-SB>
2024-09-04 13:45:50 +0100

    Add project files.

---
+++ Subroutines/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subroutines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine( Math.Pow(7,2));
+
+
+        }
+    }
+}
